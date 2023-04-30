458995e23de5045b571bcaffb152399f5e4df6f9
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-04-30 22:20:28 +0200

    First implementation

--- src/modules/recoltable_scanner.py
+++ src/modules/recoltable_scanner.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pyautogui
 from monitor import Monitor
 from ocr import OCR
+import config
+import time
+import pandas as pd
 
 class RecoltableScanner:
     
@@ -8,31 +12,44 @@ class RecoltableScanner:
         pass
     
     def init_grid_search(self, monitor: Monitor):
-        screenshot = monitor.get_clickable_game_zone()
-        shape = np.array(screenshot).shape
+        
         # Define the x and y ranges
-        x = range(30, shape[1], 40)
-        y = range(30, shape[0], 40)
+        x = range(config.P_GROUND_LEFT * monitor.width + monitor.x_offset, 
+                  config.P_GROUND_RIGHT * monitor.width + monitor.x_offset, 
+                  config.P_SCAN_X_SKIP)
+        
+        y = range(config.P_GROUND_TOP * monitor.height + monitor.y_offset, 
+                  config.P_GROUND_BOTTOM * monitor.height + monitor.y_offset, 
+                  config.P_SCAN_Y_SKIP)
+        
         # Create the grid using meshgrid
         self.X, self.Y = np.meshgrid(x, y)
         
     
-    def scan_grid(grid: list, monitor: Monitor, ocr: OCR, recolt: bool) -> list:
-        coords = []
-        for i in range(len(X)):
-            for j in range(len(X[0])):
+    def scan_grid(self, grid: list, monitor: Monitor, ocr: OCR, recolt: bool) -> pd.DataFrame:
+        x_coords = []
+        y_coords = []
+        recoltable_names = []
+        for i in range(len(self.X)):
+            for j in range(len(self.X[0])):
                 a = i 
-                b = (j if i%2==0 else len(X[0])- j - 1) 
-                x, y = X[a, b]+ init[0], Y[a, b]+init[1]
-                pyautogui.moveTo((x, y))
+                b = (j if i%2==0 else len(self.X[0])- j - 1) 
+                x, y = self.X[a, b], self.Y[a, b]
+                monitor.move_cursor(x, y)
                 time.sleep(0.3)
-              
-                black_box = get_crop_box()
-                text = get_text_box(black_box).lower()
-                if type(black_box)!= np.ndarray:
+                black_box = monitor.get_box_near_cursor_position()
+                text = ocr.recognize_text(black_box, config.ALPHABET_CHARS)
+                if type(black_box) != np.ndarray:
                     black_box.close()
-                if 'ble' in text or 'bie' in text:
-                    coords.append([x, y])
-                    if 'fauch' in text or 'puis' not in text:
-                        pyautogui.click(button='left')
-                        time.sleep(4)
+                for name in config.RECOLTABLE_NAMES:
+                    if name in text:
+                        x_coords.append(x)
+                        y_coords.append(y)
+                        recoltable_names.append(name)
+                    if config.STR_RECOLTABLE_AVAILABLE in text or config.STR_RECOLTABLE_UNAVAILABLE not in text:
+                        if recolt:
+                            monitor.click_on_mouse()
+                            time.sleep(3)
+        df = pd.DataFrame({'name': recoltable_names, 'x': x_coords, 'y': y_coords})
+        return df
+        
