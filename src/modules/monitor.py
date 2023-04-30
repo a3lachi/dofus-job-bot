c032c3a1e2f00de4be855c705ccaf456ee01aa60
a3laxi <mad.aalachi@gmail.com>
2023-05-01 00:05:14 +0200

    import solved

--- src/modules/monitor.py
+++ src/modules/monitor.py
@@ -5,74 +5,32 @@ import platform
 import os
 import importlib
 
-'''
-    Import module by its path regardless of Os
-    VERY UGLY FUNCTION THAT WAS ONCE 2 FUNCTIONS EMBEDDING BOTH IF EL
+sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')))
 
-    USE CASE : 
+import config
 
-    config = import_module('../config')
-    performance = import_module('../utils/performance')
+import pyautogui
+import Quartz.CoreGraphics as CG
+import subprocess
+from Pil import Image, ImageGrab
+import numpy as np
+import cv2 
 
-    performance.scope_time(False)   // call with module.method / module.variable
-    print(56,scope_time(True))      // call method / variable
-    print(config.P_MOUSE_MIN_HEIGHT)
-    print(P_MOUSE_MIN_HEIGHT)
-
-'''
-def import_module(module_path):
-    curr_os = platform.system()
-    add_path = '/'.join(module_path.split('/')[:-1]) if curr_os == 'Darwin' else '//'.join(module_path.split('//')[:-1]) if curr_os == 'Darwin' else ""
-    add_path = add_path.split('../')
-    curr_os = platform.system()
-    config_path = '/'.join(sys.argv[0].split('/')[:-len(add_path)])+'/'+add_path[-1] if (curr_os == 'Darwin') else '/'.join(sys.argv[0].split('//')[:-len(add_path)])+'//'+add_path[-1] if (curr_os == 'Windows') else ""
-    sys.path.append(os.path.abspath(config_path))
-    module_to_import = module_path.split('/')[-1] if curr_os == 'Darwin' else module_path.split('//')[-1] if curr_os == 'Darwin' else ""
-    module = importlib.import_module(module_to_import)
-    globals().update(vars(module))
-    return module
-
-config = import_module('../config')
-performance = import_module('../utils/performance')
-
-performance.scope_time(False)
-print(56,performance.scope_time(True))
-
-print(config.P_MOUSE_MIN_HEIGHT,performance.scope_time(True))
-
-aa = '999'
-print('This printing took this amount of time',aa,performance.scope_time(True))
-
-
-
-aa = '9999999999999999999999999999999999999999'
-print(aa,performance.scope_time(True))
-
-
-print(56)
-print(scope_time(True))
-# import pyautogui
-# import Quartz.CoreGraphics as CG
-# import subprocess
-# from Pil import Image, ImageGrab
-# import numpy as np
-# import cv2 
-
-# class Monitor : 
-#     """ 
-#       A class for all the windows open in a monitor
+class Monitor : 
+    """ 
+      A class for all the windows open in a monitor
     
-#     """
+    """
         
-#     def __init__(self, _id: int):
-#         self.id = _id
-#         # Attributes that need to be defined in subsclasses
-#         self.windows = []
-#         self.y_offset = 0
-#         self.x_offset = 0
+    def __init__(self, _id: int):
+        self.id = _id
+        # Attributes that need to be defined in subsclasses
+        self.windows = []
+        self.y_offset = 0
+        self.x_offset = 0
         
-#     def _get_dofus_windows(self):
-#         """
+    def _get_dofus_windows(self):
+        """
         Gets the Dofus windows associated with this monitor.
         """
         pass
@@ -80,136 +38,136 @@ print(scope_time(True))
     def _get_monitor_offset(self):
         pass
     
-#     def focus_on_window(self, window_id: int):
-#         self.windows[window_id].focus()
+    def focus_on_window(self, window_id: int):
+        self.windows[window_id].focus()
     
-#     def get_box(self, p_left: float, p_top: float, p_right: float, p_bottom: float) -> Image:
-#         """
-#         Screenshots the box defined by the coordinates and return an Image
+    def get_box(self, p_left: float, p_top: float, p_right: float, p_bottom: float) -> Image:
+        """
+        Screenshots the box defined by the coordinates and return an Image
 
-#         Parameters
-#         ----------
-#         p_left : float
-#             Percentage of width for left x pixel coordinate
-#         p_top : float
-#             Percentage of height for top y pixel coordinate
-#         p_right : float
-#             Percentage of width for right x pixel coordinate
-#         p_bottom : float
-#             Percentage of height for bottom y pixel coordinate
+        Parameters
+        ----------
+        p_left : float
+            Percentage of width for left x pixel coordinate
+        p_top : float
+            Percentage of height for top y pixel coordinate
+        p_right : float
+            Percentage of width for right x pixel coordinate
+        p_bottom : float
+            Percentage of height for bottom y pixel coordinate
 
-#         Returns
-#         -------
-#         Image
-#             Image of the box's screenshot
+        Returns
+        -------
+        Image
+            Image of the box's screenshot
 
-#         """
-#         # Define the region of the screen to capture as a percentage of the screen size
-#         left = int(self.width * p_left) + self.x_offset
-#         top = int(self.height * p_top) + self.y_offset
-#         right = int(self.width * p_right) + self.x_offset
-#         bottom = int(self.height * p_bottom) + self.y_offset
-#         # Define bbox
-#         bbox = (left, top, right, bottom)
-#         # Take a screenshot of the defined region
-#         img = ImageGrab.grab(bbox)
-#         return img
+        """
+        # Define the region of the screen to capture as a percentage of the screen size
+        left = int(self.width * p_left) + self.x_offset
+        top = int(self.height * p_top) + self.y_offset
+        right = int(self.width * p_right) + self.x_offset
+        bottom = int(self.height * p_bottom) + self.y_offset
+        # Define bbox
+        bbox = (left, top, right, bottom)
+        # Take a screenshot of the defined region
+        img = ImageGrab.grab(bbox)
+        return img
     
-#     def get_box_near_cursor_position(self) -> Image:
-#         """
-#         Gets a screenshot of a box near cursor position [box relative coordinates are in config.py]
+    def get_box_near_cursor_position(self) -> Image:
+        """
+        Gets a screenshot of a box near cursor position [box relative coordinates are in config.py]
     
-#         Returns
-#         -------
-#         Image
-#             Screenshot of the black box near cursor position or empty image array([[]]) if none are detected
+        Returns
+        -------
+        Image
+            Screenshot of the black box near cursor position or empty image array([[]]) if none are detected
     
-#         """
-#         screenshot = self.get_box(config.P_MOUSE_LEFT, 
-#                                   config.P_MOUSE_TOP, 
-#                                   config.P_MOUSE_RIGHT, 
-#                                   config.P_MOUSE_BOTTOM)
-#         screenshot_black_box = self._extract_black_box(screenshot)
-#         return screenshot_black_box
+        """
+        screenshot = self.get_box(config.P_MOUSE_LEFT, 
+                                  config.P_MOUSE_TOP, 
+                                  config.P_MOUSE_RIGHT, 
+                                  config.P_MOUSE_BOTTOM)
+        screenshot_black_box = self._extract_black_box(screenshot)
+        return screenshot_black_box
     
       
-#     def get_box_map_position(self) -> Image:
-#         """
-#         Gets a screenshot of a box around map position [box relative coordinates are in config.py]
+    def get_box_map_position(self) -> Image:
+        """
+        Gets a screenshot of a box around map position [box relative coordinates are in config.py]
 
-#         Returns
-#         -------
-#         Image
-#             Screenshot of the box around map position
+        Returns
+        -------
+        Image
+            Screenshot of the box around map position
 
-#         """
-#         screenshot = self.get_box(config.P_MAP_LEFT, 
-#                                   config.P_MAP_TOP, 
-#                                   config.P_MAP_RIGHT, 
-#                                   config.P_MAP_BOTTOM)
-#         return screenshot
+        """
+        screenshot = self.get_box(config.P_MAP_LEFT, 
+                                  config.P_MAP_TOP, 
+                                  config.P_MAP_RIGHT, 
+                                  config.P_MAP_BOTTOM)
+        return screenshot
     
-#     def get_clickable_game_zone(self) -> Image:
-#         """
-#         Gets a screenshot of the usable ground in the game [box relative coordiantes are in config.py]
+    def get_clickable_game_zone(self) -> Image:
+        """
+        Gets a screenshot of the usable ground in the game [box relative coordiantes are in config.py]
 
-#         Returns
-#         -------
-#         Image
-#             Screenshot of the box including usable ground in the game
+        Returns
+        -------
+        Image
+            Screenshot of the box including usable ground in the game
 
-#         """
-#         screenshot = self.get_box(config.P_GROUND_LEFT, 
-#                                   config.P_GROUND_TOP, 
-#                                   config.P_GROUND_RIGHT, 
-#                                   config.P_GROUND_BOTTOM)
-#         return screenshot
+        """
+        screenshot = self.get_box(config.P_GROUND_LEFT, 
+                                  config.P_GROUND_TOP, 
+                                  config.P_GROUND_RIGHT, 
+                                  config.P_GROUND_BOTTOM)
+        return screenshot
     
-#     def _extract_black_box(self, image: Image) -> Image:
-#         """
-#         Exctracts the most prominent black box in the image
+    def _extract_black_box(self, image: Image) -> Image:
+        """
+        Exctracts the most prominent black box in the image
 
-#         Parameters
-#         ----------
-#         image : Image
-#             An Image where a black box should be included
+        Parameters
+        ----------
+        image : Image
+            An Image where a black box should be included
 
-#         Returns
-#         -------
-#         Image
-#             Image f the black box inside the input image
+        Returns
+        -------
+        Image
+            Image f the black box inside the input image
 
-#         """
-#         # Convert the PIL image to a numpy array
-#         image = np.array(image)
-#         #plt.imshow(image)
-#         # Convert the numpy array to a cv2 image
-#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-#         # Convert the image to grayscale
-#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#         # Define the lower and upper boundaries of the black color
-#         lower = np.array([0, 0, 0], dtype=np.uint8)
-#         upper = np.array([30, 30, 30], dtype=np.uint8)
-#         # Create a mask of the black pixels
-#         mask = cv2.inRange(image, lower, upper)
-#         # Find the contours of the black box
-#         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#         try:
-#             # Find the bounding box of the black box
-#             x, y, w, h = cv2.boundingRect(contours[0])
-#             # Extract the black box from the original image using the bounding box
-#             black_box = image[y:y+h, x:x+w]
-#         except:
-#             black_box=np.array([[]])
-#         return black_box
+        """
+        # Convert the PIL image to a numpy array
+        image = np.array(image)
+        #plt.imshow(image)
+        # Convert the numpy array to a cv2 image
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Define the lower and upper boundaries of the black color
+        lower = np.array([0, 0, 0], dtype=np.uint8)
+        upper = np.array([30, 30, 30], dtype=np.uint8)
+        # Create a mask of the black pixels
+        mask = cv2.inRange(image, lower, upper)
+        # Find the contours of the black box
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        try:
+            # Find the bounding box of the black box
+            x, y, w, h = cv2.boundingRect(contours[0])
+            # Extract the black box from the original image using the bounding box
+            black_box = image[y:y+h, x:x+w]
+        except:
+            black_box=np.array([[]])
+        return black_box
         
 
 class MonitorMac(Monitor):
     
-#     def __init__(self, _id: int):
-#         super().__init__(_id)
-#         self._get_dofus_windows()
-#         self._get_monitor_offset()
+    def __init__(self, _id: int):
+        super().__init__(_id)
+        self._get_dofus_windows()
+        self._get_monitor_offset()
         
     def _get_dofus_windows(self):        
         windows_snapshot = Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionAll, Quartz.kCGNullWindowID)
