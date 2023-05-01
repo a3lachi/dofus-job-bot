b93a15a65d5e0c594665d03b73fc7f2c40560067
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-05-01 02:48:27 +0200

    Implementing Action

--- src/modules/action.py
+++ src/modules/action.py
@@ -1,55 +1,51 @@
-
-
-
+from ui_handler import UIHandler
+import sys, os
+sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))) ; import config
 
 
 class Action :
     
-  def __init__(self) -> None:
-    pass
+    def __init__(self):
+        pass
 
-  def do(self)-> bool :
-    pass
+    def do(self, ui_hanlder: UIHandler)-> bool :
+        pass
 
+class ClickOnCoords(Action) : 
 
+    def __init__(self, coord_x: int, coord_y: int)->None :
+        super().__init__()
+        self.coord_x = coord_x
+        self.coord_y = coord_y
 
-class MoveArrow(Action) : 
-
-  def __init__(self, arrow: str)->None :
-    super().__init__()
-    self.arrow = arrow
-
-  def do(self)->bool :
-    print('[x] - MOVING WITH ARROW')
-    return False
-
-
-
-class MoveCoords(Action) : 
-
-  def __init__(self, x: int,y:int)->None :
-    super().__init__()
-    self.x = x
-    self.y = y
-
-  def do(self)->bool :
-    return False
-
-
-
-  
-
+    def do(self, ui_handler: UIHandler) -> bool :
+        return ui_handler.click_on_pixel(self.coord_x, self.coord_y)
     
-
-
-
-
-
-
-character = Character()
-character.execute_Action(MoveArrow("Left"))
-character.execute_Action(MoveCoords(45,34))
-character.execute_Agitction(Action("Recole Ble"))
-
-character.execute_strategy([MoveArrow("Left")])
-character.execute_strategy([MoveArrow("Left"),MoveArrow("Right"),MoveArrow("Top"),MoveArrow("Left")])
+class MoveTopMapPosition(Action):
+    
+    def __init__(self, start: list, destination: list):
+        self.start = start
+        self.destination = destination
+    
+    def _get_direction(self) -> list:
+        dx, dy = self.destination[0] - self.start[0], self.destionation[1] - self.start[1]
+        direction = [abs(dx), abs(dy)].index(max(abs(dx), abs(dy)))
+        monitor = self.ui_handler.monitor
+        if direction==0:
+            if dx==0:
+                return []
+            elif dx > 0:
+                return config.RIGHT * monitor.width + monitor.x_offset
+            else:
+                return config.LEFT * monitor.width + monitor.x_offset
+        else:
+            if dy==0:
+                return []
+            elif dy > 0:
+                return config.DOWN * monitor.height + monitor.y_offset
+            else:
+                return config.UP * monitor.height + monitor.y_offset
+    
+    def do(self, ui_handler: UIHandler):
+        direction = self._get_direction()
+        self.ui_handler.click_on_pixel(direction[0], direction[1])
